5988b1c5f8fe6065e67a4af69c3060aeac9b39c1
Marco Settanni <marcosettanni08@gmail.com>
2020-04-25 15:33:26 +0200

    Project created
    Proetto creato in Unity

---
+++ Assets/Scripts/BubbleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(0,1.0f,0);
+        transform.position += direction * Time.deltaTime * speed;
+    }
+}
