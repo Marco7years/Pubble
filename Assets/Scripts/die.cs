122d0064b1c0d3f88528f30adc4fb466b7150075
Marco Settanni <marcosettanni08@gmail.com>
2020-06-01 11:04:44 +0200

    ignore

---
+++ Assets/Scripts/die.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class die : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+
+    public void Die()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
