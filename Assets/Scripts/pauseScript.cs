9338bce05368ef4ff94a08045b4d5cd5c2b21609
Marco Settanni <marcosettanni08@gmail.com>
2020-07-11 14:00:07 +0200

    pausing
    Co-Authored-By: simochilo <62668255+simochilo@users.noreply.github.com>
    Co-Authored-By: simocap23 <62670501+simocap23@users.noreply.github.com>
    Co-Authored-By: pirkk <62700658+pirkk@users.noreply.github.com>

---
+++ Assets/Scripts/pauseScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseScript : MonoBehaviour
+{
+    bool isPaused = false;
+ 
+    /*
+    public void pauseGame()
+    {
+        Debug.Log("Is working");
+
+        if (isPaused)
+        {
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }*/
+
+    void onMouseDown()
+    {
+       if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Time.timeScale = 1f;
+                isPaused = false;
+            }
+            else
+            {
+                Time.timeScale = 0f;
+                isPaused = true;
+            }
+        }
+    }
+}
